fb6e9cc4fa5303f928d4616f40421186a846acd1
Diveyez <nemofbaltimore@gmail.com>
2018-03-10 20:54:12 -0500

    COMMIT

--- discord-install.sh
+++ discord-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # DISCORD INSTALL SCRIPT FOR UBUNTU AND DEBIAN
+#
 cd ~/
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo apt install libappindicator1 libc++1 libdbusmenu-gtk4 libindicator7 -y
 sudo dpkg -i ~/discord.deb
 echo "Done, Discord should be installed."
-
