21e7f76fb20a50f3dadf131892896dc46ba25c4c
Diveyez <nemofbaltimore@gmail.com>
2018-03-18 03:33:14 -0400

    Update discord-install.sh

--- discord-install.sh
+++ discord-install.sh
@@ -5,4 +5,5 @@ cd ~/
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo apt install libappindicator1 libc++1 libdbusmenu-gtk4 libindicator7 -y
 sudo dpkg -i ~/discord.deb
+rm discord.deb
 echo "Done, Discord should be installed."
